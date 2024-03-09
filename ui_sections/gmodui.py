662d3bbd9f88d45134b73179b89f0d84f2873ac3
Onan Chew <onanchew@gmail.com>
2024-03-09 02:08:40 -0500

    Multi Image Canididate (#28)
    * Multi Image Canididate
    - Added multiple image/Material output
    - 4.0 blender fix
    - add dictionary stuff, fix bad arm twisting
    - stop no polygon crash
    - Added register wraps
    - Redo the entire bake ui
    - automatic detection of garry's mod
    - remove missing no. users should set a name
    - added t-pose warning for users
    - clear up some todos and add some missing error dialogues
    - fix the auto gmod detection
    - add gmod not found warning
    - got rid of platform detect debug code
    - redid some descriptions
    - added a lot of translation keys
    - added gmod toggleable bodygroups menu

---
+++ ui_sections/gmodui.py
@@ -0,0 +1,90 @@
+import bpy
+import addon_utils
+
+from .. import bake as Bake
+from ..properties import get_steam_library
+from ..tools import t, get_meshes_objects, get_armature
+from ..tools import GenerateTwistBones, TwistTutorialButton, SmartDecimation, RepairShapekeys
+from ..tools import AutoDecimatePresetGood, AutoDecimatePresetQuest, AutoDecimatePresetExcellent
+from ..tools import FitClothes, SRanipal_Labels, has_shapekeys, get_shapekeys_ft, materials_list_update
+
+from bpy.types import UIList, Operator, Panel
+from bpy_extras.io_utils import ImportHelper
+
+from ..class_register import wrapper_registry
+from ..ui import register_ui_tab
+
+button_height = 1
+
+@wrapper_registry
+class Objects_Gmod_Visibility_UL_List(UIList):
+    bl_label = ""
+    
+    def filter_items(self, context, data, propname):
+        
+        ordered = []
+        objects = getattr(data, propname)
+        filtered = [self.bitflag_filter_item] * len(objects)
+        
+        
+        for k,i in enumerate(objects):
+            filtered[k] &= self.bitflag_filter_item if i.type == "MESH" else ~self.bitflag_filter_item
+            ordered.append(k)
+            #print(filtered[k])
+        ret = (filtered,ordered)
+        return ret
+    
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+        custom_icon = "OBJECT_DATAMODE"
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            row = layout.row()
+            col = row.column()
+            col.label(text=item.name, icon = custom_icon)
+            col = row.column()
+            col.prop(item, "gmod_is_toggleable")
+            col = row.column()
+            col.prop(item, "gmod_shown_by_default")
+        elif self.layout_type in {'GRID'}:
+            layout.alignment = 'CENTER'
+            layout.label(text="", icon = custom_icon)
+
+#Making a class that looks like a blender panel just to use it to cut the code up for tabs
+#This is kinda a bad look but at least it makes the UI nice! - @989onan
+
+@register_ui_tab
+class GmodPanel:
+    bl_label = t('BakePanel.gmod_options.label')
+    bl_enum = "GMOD"
+    bl_description = t('BakePanel.gmod_options.label')
+    icon = "EVENT_G"
+    
+    
+    def poll(cls, context):
+        try:
+            return context.scene.bake_platforms[context.scene.bake_platform_index].export_format == "GMOD"
+        except:
+            return False
+    
+    def draw_panel(main_panel, context, col):
+        
+        
+        item = context.scene.bake_platforms[context.scene.bake_platform_index]
+        if get_steam_library(None):
+            row = col.row(align=True)
+            box = row.box()
+            box.label(text=get_steam_library(None), icon="EVENT_G")
+        else:
+            row = col.row(align=True)
+            row.label(text=t('GmodPanel.gmod_not_found'), icon="ERROR")
+        row = col.row(align=True)
+        row.prop(item, "gmod_model_name", expand=True)
+        
+        col.label(text=t('GmodPanel.gmod_visibility.list_label'))
+        row = col.row()
+        #row.template_list("MESH_UL_vgroups", "", ob, "vertex_groups", ob.vertex_groups, "active_index", rows=rows)
+        row.template_list("Objects_Gmod_Visibility_UL_List", "The_Gmod_Visibility_List", context.scene, "objects", context.scene, "gmod_toggle_list_index", rows=10)
+        
+        
+        
+        
+        
---
+++ ui_sections/warnings.py
@@ -0,0 +1,160 @@
+import bpy
+import addon_utils
+import mathutils
+
+from .. import bake as Bake
+from ..properties import get_steam_library
+from ..tools import t, get_meshes_objects, get_armature, simplify_bonename, bone_names
+from ..tools import GenerateTwistBones, TwistTutorialButton, SmartDecimation, RepairShapekeys
+from ..tools import AutoDecimatePresetGood, AutoDecimatePresetQuest, AutoDecimatePresetExcellent
+from ..tools import FitClothes, SRanipal_Labels, has_shapekeys, get_shapekeys_ft, materials_list_update
+
+from ..ui import register_ui_tab
+
+#Making a class that looks like a blender panel just to use it to cut the code up for tabs
+#This is kinda a bad look but at least it makes the UI nice! - @989onan
+
+@register_ui_tab
+class Bake_PT_warnings:
+    bl_enum = "WARNINGS"
+    bl_label = t('BakePanel.WarningPanel.label')
+    bl_description = t('BakePanel.WarningPanel.desc') 
+    icon = "ERROR"
+    
+    def poll(cls, context):
+        return True
+    
+    def draw_panel(main_panel, context, col):
+        has_error = False
+        # Warnings. Ideally these should be dynamically generated but only take up a limited number of rows
+        if main_panel.non_node_mat_names:
+            has_error = True
+            row = col.row(align=True)
+            row.label(text="The following materials do not use nodes!", icon="ERROR")
+            row = col.row(align=True)
+            row.label(text="Ensure they have Use Nodes checked in their properties or Bake will not run.", icon="BLANK1")
+            for name in main_panel.non_node_mat_names:
+                row = col.row(align=True)
+                row.label(text=name, icon="MATERIAL")
+        if main_panel.non_bsdf_mat_names:
+            has_error = True
+            row = col.row(align=True)
+            row.label(text="The following materials do not use Principled BSDF!", icon="INFO")
+            row = col.row(align=True)
+            row.label(text="Bake may have unexpected results.", icon="BLANK1")
+            for name in main_panel.non_bsdf_mat_names:
+                row = col.row(align=True)
+                row.separator()
+                row.label(text=name, icon="MATERIAL")
+        if main_panel.multi_bsdf_mat_names:
+            has_error = True
+            row = col.row(align=True)
+            row.label(text="The following materials have multiple Principled BSDF!", icon="INFO")
+            row = col.row(align=True)
+            row.label(text="Bake may have unexpected results.", icon="BLANK1")
+            for name in main_panel.multi_bsdf_mat_names:
+                row = col.row(align=True)
+                row.separator()
+                row.label(text=name, icon="MATERIAL")
+        if main_panel.empty_material_slots:
+            has_error = True
+            row = col.row(align=True)
+            row.label(text="The following objects have no material.", icon="INFO")
+            row = col.row(align=True)
+            row.label(text="Please assign one before continuing.", icon="BLANK1")
+            for name in main_panel.empty_material_slots:
+                row = col.row(align=True)
+                row.separator()
+                row.label(text=name, icon="OBJECT_DATA")
+        if main_panel.non_world_scale_names:
+            has_error = True
+            row = col.row(align=True)
+            row.label(text="The following objects do not have scale applied", icon="INFO")
+            row = col.row(align=True)
+            row.label(text="The resulting islands will be inversely scaled.", icon="BLANK1")
+            for name in main_panel.non_world_scale_names:
+                row = col.row(align=True)
+                row.separator()
+                row.label(text=name + ": " + "{:.1f}".format(1.0/bpy.data.objects[name].scale[0]) + "x", icon="OBJECT_DATA")
+        if main_panel.too_many_uvmaps:
+            has_error = True
+            row = col.row(align=True)
+            row.label(text="The following objects have too many UVMaps!", icon="ERROR")
+            row = col.row(align=True)
+            row.label(text="Bake will likely fail, you can have at most 6 maps.", icon="BLANK1")
+            for name in main_panel.too_many_uvmaps:
+                row = col.row(align=True)
+                row.separator()
+                row.label(text=name + ": " + "{}".format(len(bpy.data.objects[name].data.uv_layers)), icon="OBJECT_DATA")
+        if any(item.export_format == "GMOD" for item in context.scene.bake_platforms):
+            has_gmod_error = False
+            has_polygon_error = False
+            for obj in get_meshes_objects(context):
+                #print(len(obj.data.polygons))
+                if len(obj.data.polygons) > 9900:
+                    has_gmod_error = True
+                    has_polygon_error = True
+                    row = col.row(align=True)
+                    row.label(text=obj.name+" Has more than 9900 polygons.", icon="MESH_DATA")
+            if has_polygon_error:
+                row = col.row(align=True)
+                col2 = row.column(align=True)
+                col2.label(text="To keep under the hard limit of 10000 per mesh for Gmod,")
+                col2.label(text="meshes above 9900 will be reduced by Tuxedo during bake.")
+            armature = get_armature(context)
+            if armature:
+                reverse_bone_lookup = dict()
+                bone_arm_list = ["right_wrist","left_wrist","right_arm","left_arm","left_elbow","right_elbow"]
+                for (preferred_name, name_list) in bone_names.items():
+                    if preferred_name in bone_arm_list:
+                        for name in name_list: 
+                            reverse_bone_lookup[name] = preferred_name
+                #print("dictionary")
+                #print(reverse_bone_lookup)
+                arm_bones = {}
+                for bone in armature.data.bones:
+                    if simplify_bonename(bone.name) in reverse_bone_lookup:
+                        arm_bones[reverse_bone_lookup[simplify_bonename(bone.name)]] = bone
+                #print(arm_bones)
+                for bone_name in bone_arm_list:
+                    bone_vec = mathutils.Vector((0,1,0))
+                    bone_vec.rotate(arm_bones[bone_name].matrix_local.to_euler())
+                    bone_vec.normalize()
+                    
+                    if "_r" in bone_name or "right" in bone_name:
+                        bone_vec = bone_vec.dot(mathutils.Vector((-1,0,0)))
+                    else:
+                        bone_vec = bone_vec.dot(mathutils.Vector((1,0,0)))
+                    
+                    
+                    if 1-bone_vec > .005:
+                        has_gmod_error = True
+                        row = col.row(align=True)
+                        row.label(text="Your "+arm_bones[bone_name].name+" bone in edit mode is not in t-pose! difference:"+str(round((1-bone_vec)*1000)/10)+"%", icon="ARMATURE_DATA")
+            
+            
+            if not get_steam_library(None):
+                row = col.row(align=True)
+                has_gmod_error = True
+                col2 = row.column(align=True)
+                col2.label(text="Tuxedo was not able to find Gmod on your machine!", icon="ERROR")
+                col2.label(text="You may be on mac, linux, or don't have gmod with steam installed.", icon="ERROR")
+            
+            if not has_gmod_error:
+                row = col.row(align=True)
+                row.label(text="No Gmod errors or warnings.", icon="CHECKMARK")
+            #add our gmod error to if we have errors, for displaying no error dialogue
+            has_error = has_gmod_error or has_error
+        
+        
+        
+        if not has_error:
+            row = col.row(align=True)
+            row.label(text="No general errors or warnings.", icon="CHECKMARK")
+            for i in range(0,5):
+                row = col.row(align=True)
+                row.label(text="")
+        
+        
+
+    
